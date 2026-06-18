fae379b73b053d39397c7646c1cbd34c9f0f3029
GambinoX <ogpuzzo@gmail.com>
2026-06-18 18:41:58 +0530

    fix: move report to correct module directory and add __init__.py

---
+++ event_bookings/event_bookings/report/event_booking_trends/event_booking_trends.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Avril Beetails and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Event Booking Trends"] = {
+	"filters": [
+
+	]
+};
